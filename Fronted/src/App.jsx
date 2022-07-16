c97824c9831e8d01b91d154cbafd9678efd80664
Shamimzz <shamimislam5091@gmail.com>
2022-07-16 12:38:01 +0600

    update

---
+++ Fronted/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  const initialCount = {
+    count: 0,
+    User: ["Shamim"],
+  };
+
+  const Increment = () => {
+    type: "increment";
+  };
+  const AddUser = () => {
+    type: "increment";
+  };
+
+  return <div className="App"></div>;
+}
+
+export default App;
