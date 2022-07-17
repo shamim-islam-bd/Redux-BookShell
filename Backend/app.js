5a73bb2f38b80d3a65e2326a3e982473cbe8cfb1
Shamimzz <shamimislam5091@gmail.com>
2022-07-17 11:17:00 +0600

    addeds

---
+++ Backend/app.js
@@ -0,0 +1,49 @@
+import { createStore } from "redux";
+
+// state 
+const initialState = {
+    products: ["tomato", "potato"],
+    count: 2,
+ }
+
+
+// declare types
+const getProducts = () =>{
+   return {
+      type: "getProducts"
+   }
+}
+const AddProducts = (value) =>{
+   return {
+      type: "AddProducts",
+      payload: value
+   }
+}
+
+// ProductsReducer
+const ProductReducer = (state = initialState, action) => {
+   switch (action.type) {
+      case "getProducts":
+       return {
+           ...state,
+        }
+      case "AddProducts":
+       return {
+           products: [...state.products, action.payload],
+           count: state.count +  1
+         }
+      default:
+         state;
+   }
+}
+    
+
+// dispatch
+const store = createStore(ProductReducer);
+
+store.subscribe(()=> {
+   console.log(store.getState())
+})
+
+store.dispatch(getProducts());
+store.dispatch(AddProducts( "banana" ));
